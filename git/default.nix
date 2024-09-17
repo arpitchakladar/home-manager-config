aba78f26619e30bdb3634104a4bbe60b18bf541a
Arpit Chakladar <54011232+arpitchakladar@users.noreply.github.com>
2024-09-17 23:35:45 +0530

    Fixed the git configuration and its credential helper.

--- git/default.nix
+++ git/default.nix
@@ -1,8 +1,14 @@
-{ ... }:
+{ lib, ... }:
 
 {
 	programs.git.enable = true;
 	programs.git.userEmail = "54011232+arpitchakladar@users.noreply.github.com";
 	programs.git.userName = "Arpit Chakladar";
-	programs.git.extraConfig.credential.helper = "store --file ~/.cache/git/credential";
+	programs.git.extraConfig = {
+		credential.helper = "store --file $HOME/.cache/git/credential";
+		core.askPass = "";
+	};
+
+	# The base directory of the credential file must exist
+	home.activation.createGitCacheDirectory = lib.hm.dag.entryAfter [ "writeBoundary" ] "mkdir -p $HOME/.cache/git";
 }
