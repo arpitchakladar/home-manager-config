2d650eaf4c57f22393b62be04e6bdff4e1ee2577
Arpit Chakladar <54011232+arpitchakladar@users.noreply.github.com>
2026-01-04 23:02:10 +0530

    Fixed so that scripts are only available if all of the required options are enabled

--- modules/scripts/default.nix
+++ modules/scripts/default.nix
@@ -15,9 +15,21 @@ ${builtins.readFile path}
 				executable = true;
 			};
 		in {
-			"scripts/system-monitor" = shellScript ./system-monitor.sh;
-			"scripts/warp-start" = shellScript ./warp-start.sh;
-			"scripts/warp-stop" = shellScript ./warp-stop.sh;
+			"scripts/system-monitor" =
+				lib.mkIf
+					(config.tools.bottom.enable &&
+					config.tools.nvtop.enable &&
+					config.tools.tmux.enable &&
+					config.tools.kitty.enable)
+				(shellScript ./system-monitor.sh);
+			"scripts/warp-start" =
+				lib.mkIf
+					config.tools.cloudflare-warp.enable
+				(shellScript ./warp-start.sh);
+			"scripts/warp-stop" =
+				lib.mkIf
+					config.tools.cloudflare-warp.enable
+				(shellScript ./warp-stop.sh);
 		};
 	};
 }
