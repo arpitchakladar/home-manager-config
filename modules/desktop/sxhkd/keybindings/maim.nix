97af7bc123fe3104ce12cb46d85aa03c4c1d676b
Arpit Chakladar <arpitchakladar@proton.me>
2026-02-04 18:29:06 +0530

    Using super + p and super + shift + p for screenshots using maim

--- modules/desktop/sxhkd/keybindings/maim.nix
+++ modules/desktop/sxhkd/keybindings/maim.nix
@@ -4,12 +4,12 @@ let
 	maim = "${pkgs.maim}/bin/maim";
 in lib.mkMerge [
 	(createKeybinding
-		"Print"
+		"p"
 		"Take screenshot of selected area and save to clipboard"
 		"${maim} -s | xclip -selection clipboard -t image/png")
 
 	(createKeybinding
-		"Shift + Print"
+		"shift + p"
 		"Take screenshot of selected area and save to ~/Pictures/Screenshots"
 		"mkdir -p ~/Pictures/Screenshots && ${maim} -s ~/Pictures/Screenshots/screenshot-$(date +%Y%m%d-%H%M%S).png")
 ]
