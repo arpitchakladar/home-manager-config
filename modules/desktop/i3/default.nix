c65a118fe4f13d3d802011070be5964be9f93d41
Arpit Chakladar <arpitchakladar@proton.me>
2026-03-13 20:55:05 +0530

    minor changes

--- modules/desktop/i3/default.nix
+++ modules/desktop/i3/default.nix
@@ -48,7 +48,7 @@
           };
         };
 
-        defaultWorkspace = "1";
+        defaultWorkspace = "workspace number 1";
 
         window.commands = [
           {
@@ -62,11 +62,6 @@
         focus.followMouse = true;
 
         startup = [
-          # {
-          #   command = "${config.services.polybar.script}";
-          #   always = true;
-          #   notification = false;
-          # }
           {
             command = "xrandr -q | grep -w 'connected' | cut -d' ' -f1 | xargs -I{} i3-msg \"workspace 1 output {}\"";
             always = false;
@@ -87,17 +82,7 @@
       };
       extraConfig = ''
         exec_always --no-startup-id xrandr -q | grep -w 'connected' | cut -d' ' -f1 | xargs -I{} i3-msg "workspace 1 output {}"
-        set $ws1  "workspace number 1"
-        set $ws2  "workspace number 2"
-        set $ws3  "workspace number 3"
-        set $ws4  "workspace number 4"
-        set $ws5  "workspace number 5"
-        set $ws6  "workspace number 6"
-        set $ws7  "workspace number 7"
-        set $ws8  "workspace number 8"
-        set $ws9  "workspace number 9"
-        set $ws10 "workspace number 10"
-        exec_always --no-startup-id "i3-msg 'workspace 1; workspace 2; workspace 3; workspace 4; workspace 5; workspace 6; workspace 7; workspace 8; workspace 9; workspace 10; workspace 1' && polybar-msg cmd restart"
+        exec_always --no-startup-id "polybar-msg cmd restart"
       '';
     };
   };
