62cd26ceabde7f7aa004f36c456bbc6b608e62b4
Arpit Chakladar <arpitchakladar@proton.me>
2026-04-01 21:56:02 +0530

    Fixed forcing of tabs in neovim

--- modules/tools/nixvim/default.nix
+++ modules/tools/nixvim/default.nix
@@ -60,16 +60,6 @@
         vim.api.nvim_set_hl(0, 'NotifyTRACEBody', { link = 'Normal' })
 
         vim.api.nvim_create_autocmd({ "FileType" }, {
-          pattern = "*",
-          callback = function()
-            vim.opt_local.expandtab = false
-            vim.opt_local.shiftwidth = 3
-            vim.opt_local.softtabstop = 3
-            vim.opt_local.tabstop = 3
-          end,
-        })
-
-        vim.api.nvim_create_autocmd({ "FileType" }, {
           pattern = { "yaml", "yml" },
           callback = function()
             vim.opt_local.expandtab = true
