a2f3c4d73c084e03393c0e4a7c8906283f1d194a
Arpit Chakladar <arpitchakladar@proton.me>
2026-04-15 15:10:52 +0530

    added comments to nixvim comments plugin

--- modules/tools/nixvim/plugins/comment.nix
+++ modules/tools/nixvim/plugins/comment.nix
@@ -4,5 +4,20 @@
 {
   config.programs.nixvim.plugins.comment = lib.mkIf config.tools.nixvim.enable {
     enable = true;
+    settings = {
+      toggler = {
+        line = "gcc"; # toggle line comment
+        block = "gbc"; # toggle block comment
+      };
+      opleader = {
+        line = "gc"; # line comment operator (gc + motion)
+        block = "gb"; # block comment operator (gb + motion)
+      };
+      extra = {
+        above = "gcO"; # add comment line above
+        below = "gco"; # add comment line below
+        eol = "gcA"; # add comment at end of line
+      };
+    };
   };
 }
