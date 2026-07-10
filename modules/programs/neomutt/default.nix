76151ef2be0768145fa1bc78ad17abbdda1b8ec7
Arpit Chakladar <arpitchakladar+git@gmail.com>
2026-07-10 15:22:31 +0530

    feat: added some neomutt macros and keybindings
    using O for running mbsync -a (for syncing the emails from the server and downloading locally)
    using Ctrl+j, Ctrl+k and Ctrl+o (for moving down, up and opening the mailboxes)

--- modules/programs/neomutt/default.nix
+++ modules/programs/neomutt/default.nix
@@ -16,6 +16,43 @@
       sidebar.enable = true;
       sort = "reverse-threads";
       vimKeys = true;
+      binds = [
+        {
+          map = [
+            "index"
+            "pager"
+          ];
+          key = "\\CK";
+          action = "sidebar-prev";
+        }
+        {
+          map = [
+            "index"
+            "pager"
+          ];
+          key = "\\CJ";
+          action = "sidebar-next";
+        }
+        {
+          map = [
+            "index"
+            "pager"
+          ];
+          key = "\\CO";
+          action = "sidebar-open";
+        }
+      ];
+
+      macros = [
+        {
+          map = [
+            "index"
+            "pager"
+          ];
+          key = "O";
+          action = "<sync-mailbox><shell-escape>mbsync -a<enter><sync-mailbox>";
+        }
+      ];
     };
   };
 }
