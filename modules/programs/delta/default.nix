e984847aa0b18ca64a1bb043724e1c3f3b36df65
Arpit Chakladar <arpitchakladar+git@gmail.com>
2026-07-14 22:43:58 +0530

    feat(delta): made some theme changes to delta
    Added line numbers and removed the hunk-header

--- modules/programs/delta/default.nix
+++ modules/programs/delta/default.nix
@@ -6,6 +6,14 @@
 # Delta - A syntax-highlighting pager for git, diff, and grep output
 {
   config = lib.mkIf config.programs.delta.enable {
+    programs.delta.options = {
+      line-numbers = true;
+      hunk-header-style = "omit";
+      hunk-header-decoration-style = "omit";
+      line-numbers-left-format = "{nm}│ ";
+      line-numbers-right-format = "{np}│ ";
+    };
+
     programs.git.settings = lib.mkIf config.programs.git.enable {
       core.pager = "delta";
       interactive.diffFilter = "delta --color-only";
