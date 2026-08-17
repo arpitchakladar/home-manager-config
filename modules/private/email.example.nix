2ac22a72455341a3efced289782fe5daf5d788ee
Arpit Chakladar <arpitchakladar+git@gmail.com>
2026-08-17 15:25:55 +0530

    refactor: rewrote how ssh keys are handled in gopass-ssh-load
    - all ssh keys that are used under ssh/ have to be explicitely added via
      security.ssh.gopassKeys = [ ... ]

--- modules/private/email.example.nix
+++ modules/private/email.example.nix
@@ -1,20 +1,22 @@
 # Email example - Template for configuring neomutt email accounts
 { ... }:
 {
-  config.communication.neomutt.accounts = {
-    "example@gmail.com" = {
-      realName = "Example User";
-      address = "user@gmail.com";
-      passwordGopassSecret = "mail/user@gmail.com";
-      flavor = "gmail.com";
-      primary = true;
-      neomutt.extraConfig = ''
-        set pgp_default_key = YOUR_GPG_KEY_FINGERPRINT
-      '';
-      gpg = {
-        key = "YOUR_GPG_KEY_ID";
-        signByDefault = true;
-        encryptByDefault = false; # set true only if you also want auto-encrypt
+  config = {
+    communication.neomutt.accounts = {
+      "example@gmail.com" = {
+        realName = "Example User";
+        address = "user@gmail.com";
+        passwordGopassSecret = "mail/user@gmail.com";
+        flavor = "gmail.com";
+        primary = true;
+        neomutt.extraConfig = ''
+          set pgp_default_key = YOUR_GPG_KEY_FINGERPRINT
+        '';
+        gpg = {
+          key = "YOUR_GPG_KEY_ID";
+          signByDefault = true;
+          encryptByDefault = false; # set true only if you also want auto-encrypt
+        };
       };
     };
   };
