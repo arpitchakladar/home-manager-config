32e02f06663c5cae15fea6681c46bffac3be0272
Arpit Chakladar <arpitchakladar+git@gmail.com>
2026-08-24 18:59:56 +0530

    desktop: added gnome portal
    - added gnome portal to use with niri

--- modules/desktop/xdg/default.nix
+++ modules/desktop/xdg/default.nix
@@ -5,13 +5,25 @@
   pkgs,
   ...
 }:
+let
+  gtkCss = builtins.readFile (
+    config.scheme {
+      template = builtins.readFile ./style.mustache.css;
+      extension = ".css";
+    }
+  );
+in
 {
   config = lib.mkIf config.desktop.enable {
+    dconf.enable = true;
+
     xdg.portal = {
       enable = true;
       extraPortals = with pkgs; [
         xdg-desktop-portal-termfilechooser
+        xdg-desktop-portal-gnome
       ];
+      configPackages = [ pkgs.niri ];
       config.common = {
         "org.freedesktop.impl.portal.FileChooser" = [ "termfilechooser" ];
         default = [
@@ -20,6 +32,35 @@
         ];
       };
       xdgOpenUsePortal = true;
+    };
+
+    gtk = {
+      enable = true;
+      font = {
+        name = config.fonts.normal;
+        size = config.fonts.size;
+      };
+      theme = {
+        name = "Adwaita-dark";
+        package = pkgs.gnome-themes-extra;
+      };
+      iconTheme = {
+        name = "Adwaita";
+        package = pkgs.adwaita-icon-theme;
+      };
+      cursorTheme = {
+        name = "Adwaita";
+        package = pkgs.adwaita-icon-theme;
+        size = 20;
+      };
+      colorScheme = "dark";
+      gtk3.extraCss = gtkCss;
+      gtk4 = {
+        # GTK 4/libadwaita does not support loading GTK 3 themes; use the
+        # Base16 CSS above without Home Manager's compatibility workaround.
+        theme = null;
+        extraCss = gtkCss;
+      };
     };
   };
 }
