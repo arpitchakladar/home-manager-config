c481a8e1ac6a4cb357f5e8e907b881b2cfe31f6e
Arpit Chakladar <arpitchakladar+git@gmail.com>
2026-08-29 18:58:09 +0530

    update: ran nix flake update, updated ublock-origin chrome extension

--- modules/web/chromium/extensions/ublock-origin/metadata.nix
+++ modules/web/chromium/extensions/ublock-origin/metadata.nix
@@ -2,11 +2,11 @@
   pname = "ublock-origin";
   owner = "gorhill";
   repo = "uBlock";
-  version = "1.73.0";
+  version = "1.74.0";
   updateType = "release";
   tagPrefix = "";
   id = "eahmphincfcmmpgkpobpneimcjndlcna";
-  url = "https://github.com/gorhill/uBlock/releases/download/1.73.0/uBlock0_1.73.0.chromium.zip";
-  hash = "sha256-IsYrwPNe0MXpXR/IMagprxmcfKb9tPhBvgNMJXGc6RE=";
+  url = "https://github.com/gorhill/uBlock/releases/download/1.74.0/uBlock0_1.74.0.chromium.zip";
+  hash = "sha256-KaR16CaIswTyqbLAV3wmVaiq91zjY/4C1yDTibu59FE=";
   extensionKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtXfkoFzWOh3Fet9x4L6WY1pw755kt7bFwqd/nmTm5Ptg7d40Qde0tTIhceCFVkwrWTQ3n2/WSpYXoU/UMgSW9anBzoUM0ZXanIdoeHwvL3eMNyx+iyvSrRxee26GwzbZ8J0UvH77KT3rW/iV5ejV4wTxTD3rFr0udkI5bbTPYpmGFOKPD8I3IB6BsoUTKW3XTSLM0fUYggdbDNwQGZvLYwqZzk/vWH/oifHR6CEvdFR51JAdf3tJQhttI6My49Vq6etCi9y5ZLXrMksxcEcIlQKaF2fdlfL1bYXRAVFDsieuAmu3gJPBj65PVJif5A7DfcH8zm8YGPwrJlTjaJQeDQIDAQAB";
 }
