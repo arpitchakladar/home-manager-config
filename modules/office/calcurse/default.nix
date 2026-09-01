e61d4b9a561893ad1cc4220fa319b1d25287048a
Arpit Chakladar <arpitchakladar+git@gmail.com>
2026-09-02 01:13:38 +0530

    feat(office/calcurse): wrapped calcurse with PAGER=nvim, todos show completed
    - using nvim as the pager for calcurse, seems to work better
    - now completed todos also appear in the todos section just at the
    bottom

--- modules/office/calcurse/default.nix
+++ modules/office/calcurse/default.nix
@@ -5,25 +5,39 @@
   pkgs,
   ...
 }:
+let
+  calcursePackage =
+    if config.development.nixvim.enable then
+      pkgs.symlinkJoin {
+        name = "calcurse-wrapped";
+        paths = [ pkgs.calcurse ];
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+          wrapProgram $out/bin/calcurse --set PAGER "nvim"
+        '';
+        meta = pkgs.calcurse.meta // {
+          mainProgram = "calcurse";
+        };
+      }
+    else
+      pkgs.calcurse;
+in
 {
   options.office.calcurse = {
     enable = lib.mkEnableOption "Enables calcurse.";
     package = lib.mkOption {
       type = lib.types.package;
       readOnly = true;
-      default = pkgs.calcurse;
+      default = calcursePackage;
       description = "The calcurse package to use.";
     };
   };
-
   config = lib.mkIf config.office.calcurse.enable {
     home.packages = [ config.office.calcurse.package ];
-
     xdg.configFile."calcurse/conf" = {
       source = ./conf;
       force = true;
     };
-
     xdg.configFile."calcurse/keys" = {
       source = ./keys;
       force = true;
