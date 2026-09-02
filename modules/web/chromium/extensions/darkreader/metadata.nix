66778d5404cfbde1b5ef7c2bc57f8fe29e2c78f4
Arpit Chakladar <arpitchakladar+git@gmail.com>
2026-09-03 03:51:59 +0530

    update(web/chromium/darkreader): bumped up to version v4.9.130

--- modules/web/chromium/extensions/darkreader/metadata.nix
+++ modules/web/chromium/extensions/darkreader/metadata.nix
@@ -2,11 +2,11 @@
   pname = "darkreader";
   owner = "darkreader";
   repo = "darkreader";
-  version = "4.9.129";
+  version = "4.9.130";
   updateType = "release";
   tagPrefix = "v";
   id = "nfkppknaehpcafkbgmhfhkpapflndfip";
-  url = "https://github.com/darkreader/darkreader/releases/download/v4.9.129/darkreader-chrome.zip";
-  hash = "sha256-Yuhgoxp5MRgVMEEbOaQvRsVjYn/iaBhcHujipBX5rUE=";
+  url = "https://github.com/darkreader/darkreader/releases/download/v4.9.130/darkreader-chrome.zip";
+  hash = "sha256-Y0HHVlK/FOZpfqWxCeM0Ob2Z8nUFKQ+WC8S9Sv9snpU=";
   extensionKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8xN0p4CHkthdkB6imIb+8T2xsNSH9eeFAy7NhP3xAO1ML6FJyOFroabQIG6HcV7miXmJamY7zmE96QgEEvGqQ18+g+RsfvriJp0LRXi95TJBdCXoMiW348bWmue0Jk+/rhzWijMn/z/5fDnrZl0xjKU7MAXzW9dWfiTeIBc4wlk21zjGJxiG0uM1YAywe3jW8g0QCBga9iTaN67D+sBbBs4HmPa3xIbtPTAgqt1LDEk2T7IseoUia8MwxATWKWFegW6legMkPLVWH9jQ+x2ZbjtDP9GXk+1YqqWQUll1Xmal7C9ljeagkM5nhxgttAQoWBMPMJFwr7VN3AXtNXXPWQIDAQAB";
 }
