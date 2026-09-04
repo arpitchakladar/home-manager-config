791b8f8cceeb74c8123927561df27356866c9d2b
Arpit Chakladar <arpitchakladar+git@gmail.com>
2026-09-05 03:48:01 +0530

    fix(desktop/eww): fixed the startup of eww-bar + eww services

--- modules/desktop/eww/default.nix
+++ modules/desktop/eww/default.nix
@@ -20,7 +20,13 @@
   config = {
     programs.eww = {
       enable = true;
-      systemd.enable = true;
+      systemd = {
+        enable = true;
+        # Nothing ever activates this, so home-manager's eww.service unit
+        # exists but is never auto-started by the session. Rely on eww-bar.service
+        # manually starts eww service.
+        target = "eww-daemon-manual.target";
+      };
       scssConfig =
         let
           baseSize = config.fonts.size;
@@ -53,16 +59,18 @@
     systemd.user.services.eww-bar = {
       Unit = {
         Description = "Open Eww Bar";
-        After = [ "eww.service" ];
-        Requires = [ "eww.service" ];
-        BindsTo = [ "eww.service" ];
+        PartOf = [ "graphical-session.target" ];
+        After = [ "graphical-session.target" ];
       };
       Service = {
         Type = "oneshot";
         RemainAfterExit = true;
         ExecStart = "${lib.getExe config.desktop.eww.package} open bar";
-        ExecStop = "${lib.getExe config.desktop.eww.package} close bar";
+        ExecReload = "${lib.getExe config.desktop.eww.package} reload && ${lib.getExe config.desktop.eww.package} open bar";
+        ExecStop = "${lib.getExe config.desktop.eww.package} kill";
+        Restart = "on-failure";
       };
+      Install.WantedBy = [ "graphical-session.target" ];
     };
   };
 }
