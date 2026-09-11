16bfed62474402f14e9476330dac55b83e598cfd
Arpit Chakladar <arpitchakladar@gmail.com>
2026-09-11 21:45:09 +0530

    feat(desktop/xdg): configured gtk to use base16 colorscheme

--- modules/desktop/xdg/default.nix
+++ modules/desktop/xdg/default.nix
@@ -5,14 +5,6 @@
   pkgs,
   ...
 }:
-let
-  gtkCss = builtins.readFile (
-    config.scheme {
-      template = builtins.readFile ./style.mustache.css;
-      extension = ".css";
-    }
-  );
-in
 {
   config = lib.mkIf config.desktop.enable {
     dconf.enable = true;
@@ -31,33 +23,42 @@ in
       xdgOpenUsePortal = true;
     };
 
-    gtk = {
-      enable = true;
-      font = {
-        name = config.fonts.normal;
-        size = config.fonts.uiSize;
+    gtk =
+      let
+        gtkCss = builtins.readFile (
+          config.scheme {
+            template = builtins.readFile ./style.mustache.css;
+            extension = ".css";
+          }
+        );
+      in
+      {
+        enable = true;
+        font = {
+          name = config.fonts.normal;
+          size = config.fonts.uiSize;
+        };
+        theme = {
+          name = "Adwaita-dark";
+          package = pkgs.gnome-themes-extra;
+        };
+        iconTheme = {
+          name = "Adwaita";
+          package = pkgs.adwaita-icon-theme;
+        };
+        cursorTheme = {
+          name = "Adwaita";
+          package = pkgs.adwaita-icon-theme;
+          size = 20;
+        };
+        colorScheme = "dark";
+        gtk3.extraCss = gtkCss;
+        gtk4 = {
+          # GTK 4/libadwaita does not support loading GTK 3 themes; use the
+          # Base16 CSS above without Home Manager's compatibility workaround.
+          theme = null;
+          extraCss = gtkCss;
+        };
       };
-      theme = {
-        name = "Adwaita-dark";
-        package = pkgs.gnome-themes-extra;
-      };
-      iconTheme = {
-        name = "Adwaita";
-        package = pkgs.adwaita-icon-theme;
-      };
-      cursorTheme = {
-        name = "Adwaita";
-        package = pkgs.adwaita-icon-theme;
-        size = 20;
-      };
-      colorScheme = "dark";
-      gtk3.extraCss = gtkCss;
-      gtk4 = {
-        # GTK 4/libadwaita does not support loading GTK 3 themes; use the
-        # Base16 CSS above without Home Manager's compatibility workaround.
-        theme = null;
-        extraCss = gtkCss;
-      };
-    };
   };
 }
