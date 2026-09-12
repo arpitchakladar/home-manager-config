b3123b2c13a039cda45e0a2b45e0e404dac8d96e
Arpit Chakladar <arpitchakladar@gmail.com>
2026-09-13 03:42:05 +0530

    feat(development/nixvim): added more comprehensive list of keybindings for lsp

--- modules/development/nixvim/plugins/lsp.nix
+++ modules/development/nixvim/plugins/lsp.nix
@@ -6,12 +6,28 @@
     inlayHints = true;
     autoLoad = true;
 
-    keymaps.lspBuf = {
-      K = "hover";
-      gD = "references";
-      gd = "definition";
-      gi = "implementation";
-      gt = "type_definition";
+    keymaps = {
+      lspBuf = {
+        "<leader>lh" = "hover";
+        "<leader>lr" = "references";
+        "<leader>ld" = "definition";
+        "<leader>li" = "implementation";
+        "<leader>lt" = "type_definition";
+        "<leader>lD" = "declaration";
+        "<leader>lR" = "rename";
+        "<leader>la" = "code_action";
+        "<leader>lf" = "format";
+        "<leader>ls" = "signature_help";
+        "<leader>lwa" = "add_workspace_folder";
+        "<leader>lwr" = "remove_workspace_folder";
+        "<leader>lwl" = "list_workspace_folders";
+      };
+      diagnostic = {
+        "<leader>le" = "open_float";
+        "<leader>lq" = "setloclist";
+        "[d" = "goto_prev";
+        "]d" = "goto_next";
+      };
     };
 
     servers = {
