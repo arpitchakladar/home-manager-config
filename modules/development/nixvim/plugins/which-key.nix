54102de5c057ece86fa4f173ac65df2d6f202869
Arpit Chakladar <arpitchakladar@gmail.com>
2026-09-15 03:39:39 +0530

    feat(development/nixvim): added keybinding groups to which-key

--- modules/development/nixvim/plugins/which-key.nix
+++ modules/development/nixvim/plugins/which-key.nix
@@ -14,6 +14,30 @@
       win = {
         border = "single";
       };
+
+      # Group descriptions for leader prefixes
+      spec = [
+        {
+          __unkeyed-1 = "<leader>g";
+          group = "+Git";
+        }
+        {
+          __unkeyed-1 = "<leader>f";
+          group = "+Find";
+        }
+        {
+          __unkeyed-1 = "<leader>d";
+          group = "+Debug";
+        }
+        {
+          __unkeyed-1 = "<leader>l";
+          group = "+LSP";
+        }
+        {
+          __unkeyed-1 = "<leader>c";
+          group = "+Comment";
+        }
+      ];
     };
   };
 }
