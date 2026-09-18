453c4a6002e51265f5bcaef3bf53a1362f4c7b6a
Arpit Chakladar <arpitchakladar@gmail.com>
2026-09-18 05:56:06 +0530

    chore(communication/neomutt): some minor refactoring

---
+++ modules/communication/neomutt/account/assertions.nix
@@ -0,0 +1,30 @@
+# Validates the neomutt account configurations
+{ config, lib, ... }:
+{
+  config.assertions = [
+    {
+      assertion =
+        !config.communication.neomutt.enable
+        || !lib.any (account: account.enable && account.passwordGopassSecret != null) (
+          lib.attrValues config.communication.neomutt.accounts
+        )
+        || config.security.gopass.enable;
+      message = ''
+        An enabled communication.neomutt account uses passwordGopassSecret but security.gopass.enable is not set.
+        Enable security.gopass to provide the account password command.
+      '';
+    }
+    {
+      assertion =
+        !config.communication.neomutt.enable
+        || !lib.any (account: account.enable && account.gpg.key != null) (
+          lib.attrValues config.communication.neomutt.accounts
+        )
+        || config.security.gpg.enable;
+      message = ''
+        An enabled communication.neomutt account specifies a GPG key but security.gpg.enable is not set.
+        Enable security.gpg to provide mail signing and encryption support.
+      '';
+    }
+  ];
+}
--- modules/communication/neomutt/account/default.nix
+++ modules/communication/neomutt/account/default.nix
@@ -1,6 +1,10 @@
 # Converts neomutt account options into home-manager email config
 { config, lib, ... }:
 {
+  imports = [
+    ./assertions.nix
+  ];
+
   options.communication.neomutt.accounts = lib.mkOption {
     type = lib.types.attrsOf (
       lib.types.submodule {
@@ -15,117 +19,94 @@
     description = "Specification of email accounts.";
   };
 
-  config = {
-    assertions = [
-      {
-        assertion =
-          !config.communication.neomutt.enable
-          || !lib.any (account: account.enable && account.passwordGopassSecret != null) (
-            lib.attrValues config.communication.neomutt.accounts
-          )
-          || config.security.gopass.enable;
-        message = ''
-          An enabled communication.neomutt account uses passwordGopassSecret but security.gopass.enable is not set.
-          Enable security.gopass to provide the account password command.
-        '';
+  config = lib.mkIf config.communication.neomutt.enable {
+    accounts.email.accounts = lib.mapAttrs (accountName: accountInformation: {
+      inherit (accountInformation) realName address primary;
+
+      userName =
+        if accountInformation.userName != null then
+          accountInformation.userName
+        else
+          accountInformation.address;
+
+      passwordCommand =
+        if accountInformation.passwordGopassSecret != null then
+          "${lib.getExe config.security.gopass.package} -o ${accountInformation.passwordGopassSecret}"
+        else
+          null;
+
+      flavor = accountInformation.flavor;
+      inherit (accountInformation) aliases;
+
+      folders = {
+        inherit (accountInformation.folders) inbox trash;
       }
-      {
-        assertion =
-          !config.communication.neomutt.enable
-          || !lib.any (account: account.enable && account.gpg.key != null) (
-            lib.attrValues config.communication.neomutt.accounts
-          )
-          || config.security.gpg.enable;
-        message = ''
-          An enabled communication.neomutt account specifies a GPG key but security.gpg.enable is not set.
-          Enable security.gpg to provide mail signing and encryption support.
-        '';
+      // lib.optionalAttrs (accountInformation.folders.drafts != null) {
+        drafts = accountInformation.folders.drafts;
       }
-    ];
+      // lib.optionalAttrs (accountInformation.folders.sent != null) {
+        sent = accountInformation.folders.sent;
+      };
 
-  }
-  // lib.mkIf config.communication.neomutt.enable {
-    accounts.email.accounts = lib.mapAttrs (
-      name: account:
-      let
-        a = account;
-      in
-      {
-        inherit (a) realName address primary;
-        userName = if a.userName != null then a.userName else a.address;
+      neomutt = {
+        enable = true;
+        mailboxType = accountInformation.neomutt.mailboxType;
+        extraConfig = accountInformation.neomutt.extraConfig;
+      };
 
-        passwordCommand =
-          if a.passwordGopassSecret != null then
-            "${lib.getExe config.security.gopass.package} -o ${a.passwordGopassSecret}"
-          else
-            null;
+      mbsync = {
+        enable = true;
+        create = accountInformation.mbsync.create;
+        patterns = accountInformation.mbsync.patterns;
+        flatten = accountInformation.mbsync.flatten;
+        extraConfig = {
+          account = accountInformation.mbsync.extraConfig.account;
+          channel = accountInformation.mbsync.extraConfig.channel;
+          local = accountInformation.mbsync.extraConfig.local;
+          remote = accountInformation.mbsync.extraConfig.remote;
+        };
+      };
 
-        flavor = a.flavor;
-        inherit (a) aliases;
-
-        folders = {
-          inherit (a.folders) inbox trash;
-        }
-        // lib.optionalAttrs (a.folders.drafts != null) { drafts = a.folders.drafts; }
-        // lib.optionalAttrs (a.folders.sent != null) { sent = a.folders.sent; };
-
+      notmuch = {
+        enable = true;
         neomutt = {
           enable = true;
-          mailboxType = a.neomutt.mailboxType;
-          extraConfig = a.neomutt.extraConfig;
+          virtualMailboxes = accountInformation.notmuch.neomutt.virtualMailboxes;
         };
+      };
 
-        mbsync = {
-          enable = true;
-          create = a.mbsync.create;
-          patterns = a.mbsync.patterns;
-          flatten = a.mbsync.flatten;
-          extraConfig = {
-            account = a.mbsync.extraConfig.account;
-            channel = a.mbsync.extraConfig.channel;
-            local = a.mbsync.extraConfig.local;
-            remote = a.mbsync.extraConfig.remote;
+      imap = lib.optionalAttrs (accountInformation.imap.host != null) {
+        host = accountInformation.imap.host;
+        port = accountInformation.imap.port;
+        tls = {
+          enable = accountInformation.imap.tls.enable;
+          useStartTls = accountInformation.imap.tls.useStartTls;
+        };
+      };
+
+      smtp = lib.optionalAttrs (accountInformation.smtp.host != null) {
+        host = accountInformation.smtp.host;
+        port = accountInformation.smtp.port;
+        tls = {
+          enable = accountInformation.smtp.tls.enable;
+          useStartTls = accountInformation.smtp.tls.useStartTls;
+        };
+      };
+
+      gpg = lib.optionalAttrs (accountInformation.gpg.key != null) {
+        key = accountInformation.gpg.key;
+        signByDefault = accountInformation.gpg.signByDefault;
+        encryptByDefault = accountInformation.gpg.encryptByDefault;
+      };
+
+      signature =
+        lib.optionalAttrs
+          (accountInformation.signature.text != null || accountInformation.signature.command != null)
+          {
+            text = accountInformation.signature.text;
+            command = accountInformation.signature.command;
+            showSignature = accountInformation.signature.showSignature;
           };
-        };
-
-        notmuch = {
-          enable = true;
-          neomutt = {
-            enable = true;
-            virtualMailboxes = a.notmuch.neomutt.virtualMailboxes;
-          };
-        };
-
-        imap = lib.optionalAttrs (a.imap.host != null) {
-          host = a.imap.host;
-          port = a.imap.port;
-          tls = {
-            enable = a.imap.tls.enable;
-            useStartTls = a.imap.tls.useStartTls;
-          };
-        };
-
-        smtp = lib.optionalAttrs (a.smtp.host != null) {
-          host = a.smtp.host;
-          port = a.smtp.port;
-          tls = {
-            enable = a.smtp.tls.enable;
-            useStartTls = a.smtp.tls.useStartTls;
-          };
-        };
-
-        gpg = lib.optionalAttrs (a.gpg.key != null) {
-          key = a.gpg.key;
-          signByDefault = a.gpg.signByDefault;
-          encryptByDefault = a.gpg.encryptByDefault;
-        };
-
-        signature = lib.optionalAttrs (a.signature.text != null || a.signature.command != null) {
-          text = a.signature.text;
-          command = a.signature.command;
-          showSignature = a.signature.showSignature;
-        };
-      }
-    ) (lib.filterAttrs (n: a: a.enable) config.communication.neomutt.accounts);
+    }) (lib.filterAttrs (n: a: a.enable) config.communication.neomutt.accounts);
   };
 }
